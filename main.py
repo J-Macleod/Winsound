2c4179be1bb12e5cfe18d74f0f63825b552cb5ab
Jacob Macleod <94126853+J-Macleod@users.noreply.github.com>
2023-08-08 11:48:42 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import winsound
+duration = 200  # Set Duration To 1000 ms == 1 second
+up = True
+while up:
+    input("Play?")
+    winsound.Beep(1500, duration)
+    winsound.Beep(1750, duration)
+    winsound.Beep(2000, duration)
+    winsound.Beep(2000, duration)
+    winsound.Beep(50, duration)
+    winsound.Beep(1250, duration)
+    winsound.Beep(1500, duration)
+    winsound.Beep(2000, duration)
+    input("Play next?")
+    winsound.Beep(2000, duration)
+    winsound.Beep(2000, duration)
+    winsound.Beep(1500, duration)
+    winsound.Beep(1250, duration)
+    winsound.Beep(2000, duration)
+    winsound.Beep(2000, duration)
+    winsound.Beep(1500, duration)
+    winsound.Beep(1250, duration)
